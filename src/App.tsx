801a12b873aa41065d76638c74ec67063ab32be6
Henrique Oliveira <henrique.oliveira@devbot.com.br>
2026-04-24 18:40:47 +0000

    Initial commit: CoverDrop - Vite + React + TypeScript

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-gray-900">CoverDrop</h1>
+    </div>
+  )
+}
+
+export default App
